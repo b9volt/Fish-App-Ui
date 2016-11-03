ff20a21ce4ff7cbe02ac4bc294166e44b24c5a10
Tom Murphree <tmurph1810@gmail.com>
2016-11-03 09:30:06 -0500

    Hello world is working

---
+++ server.js
@@ -0,0 +1,15 @@
+
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var app = express();
+var moment = require('moment-timezone');
+var port = process.env.PORT || 3000;
+
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname,'public')))
+
+app.listen(port, function() {
+  console.info('Server is responding on port ' + port + ' ',
+  moment(new Date(), 'America/Chicago').format());
+});
