fa47779ed6a3399639d18674db46f848e5444b56
Tom Murphree <tmurph1810@gmail.com>
2016-11-03 15:16:01 -0500

    Get Angular talking to the database

--- public/js/tripsController.js
+++ public/js/tripsController.js
@@ -6,12 +6,23 @@
 
   function TripsController($http, $window, $location) {
     var self = this;
+    var rootUrl = 'http://localhost:3000/users/1/trips'
 
-    this.tripData = null;
+    this.tripData = [];
 
-    this.getFishingTrips = function(){
 
+    getFishingTrips = function(){
+      $http.get(rootUrl)
+        .then(function(response) {
+          self.tripData = response.data.trip;
+        })
+        .catch(function(err) {
+          console.log('err',err);
+        });
     };
 
+
+    getFishingTrips();
+
   } //end TripsController
-})()
+})();
