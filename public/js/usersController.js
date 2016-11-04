bdfa0c30c0b9e127f48a1cdf9e92c04fe3f87b56
Tom Murphree <tmurph1810@gmail.com>
2016-11-04 10:19:00 -0500

    Get sign up to work

--- public/js/usersController.js
+++ public/js/usersController.js
@@ -6,7 +6,26 @@
 
   function UsersController($http, $window, $location) {
 
+    var self = this;
+    var rootUrl = 'http://localhost:3000/'
+
     this.test = 'Hello world from UsersController';
+
+    this.signup = function() {
+      console.log('Hello from users.signup -- fthis');
+      console.log('data from the model is', self.signupUsername);
+
+      $http.post(rootUrl + '/users',
+        { user: {username: self.signupUsername, password: self.signupPassword }}
+      )
+      .then(function(response) {
+        console.log(response.data);
+      })
+      .catch(function(err) {
+        console.log('err',err);
+      });
+
+    }; //this.signup
   }
     /*
     POST	/users/login
